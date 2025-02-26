53e30e1c612100802da5e407917079fc10a44cb3
Jack Whitehead <overzero@gmail.com>
2025-02-26 21:29:10 +0000

    Use requiredModId and BaseMod.ReflectionHacks
    Use requiredModId = "CardFilterMod" to ensure mod is loaded before patching it.
    Use BaseMod.ReflectionHacks to access the private CardLibrary.curses field.
    
    Use final keyword more consistently.
    Use a new naming convention for patches.

--- src/main/java/com/dvide/cardFilterCurseFix/patches/FixRemoveCurseCrash.java
+++ src/main/java/com/dvide/cardFilterCurseFix/patches/FixRemoveCurseCrash.java
@@ -1,5 +1,6 @@
 package com.dvide.cardFilterCurseFix.patches;
 
+import basemod.ReflectionHacks;
 import cardFilter.CardFilterMod;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
@@ -9,47 +10,43 @@ import com.megacrit.cardcrawl.helpers.CardLibrary;
 import javassist.CannotCompileException;
 import javassist.CtBehavior;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 @SuppressWarnings("unused")
 public class FixRemoveCurseCrash {
-    @SpirePatch2(clz = CardFilterMod.class, method = "removeCardFromPool")
-    public static class RemoveCardFromPoolPatches {
-        @SpireInsertPatch(locator = CurseCardPoolAccessLocator.class)
-        public static void removeCurseByRarityFix(String cardName) throws NoSuchFieldException, IllegalAccessException {
-            removeCardFromLibraryCurses(cardName);
+    @SpirePatch2(clz = CardFilterMod.class, method = "removeCardFromPool", requiredModId = "CardFilterMod")
+    public static class CardFilterMod_removeCardFromPool_Patches {
+        @SpireInsertPatch(locator = AbstractDungeon_curseCardPool_Locator.class)
+        public static void removeFromCardLibraryCursesByRarity(final String cardName) {
+            removeFromCardLibraryCurses(cardName);
         }
 
-        private static class CurseCardPoolAccessLocator extends SpireInsertLocator {
-            public int[] Locate(CtBehavior ctBehavior) throws CannotCompileException, PatchingException {
-                Matcher fieldMatcher = new Matcher.FieldAccessMatcher(AbstractDungeon.class, "curseCardPool");
-                return LineFinder.findInOrder(ctBehavior, fieldMatcher);
+        private static class AbstractDungeon_curseCardPool_Locator extends SpireInsertLocator {
+            public int[] Locate(final CtBehavior ctBehavior) throws CannotCompileException, PatchingException {
+                final Matcher fieldAccessMatcher = new Matcher.FieldAccessMatcher(AbstractDungeon.class, "curseCardPool");
+                return LineFinder.findInOrder(ctBehavior, fieldAccessMatcher);
             }
         }
 
-        @SpireInsertPatch(locator = CardColorAccessLocator.class, localvars={"card"})
-        public static void removeCurseByColorFix(String cardName, @ByRef AbstractCard[] card) throws NoSuchFieldException, IllegalAccessException {
+        @SpireInsertPatch(locator = AbstractCard_color_Locator.class, localvars={"card"})
+        public static void removeFromCardLibraryByColorAndType(final String cardName, @ByRef final AbstractCard[] card) {
             if (card[0].color == AbstractCard.CardColor.CURSE || card[0].type == AbstractCard.CardType.CURSE) {
                 AbstractDungeon.curseCardPool.removeCard(card[0]);
                 AbstractDungeon.srcCurseCardPool.removeCard(card[0]);
-                removeCardFromLibraryCurses(cardName);
+                removeFromCardLibraryCurses(cardName);
             }
         }
 
-        private static class CardColorAccessLocator extends SpireInsertLocator {
-            public int[] Locate(CtBehavior ctBehavior) throws CannotCompileException, PatchingException {
-                Matcher fieldMatcher = new Matcher.FieldAccessMatcher(AbstractCard.class, "color");
-                return LineFinder.findInOrder(ctBehavior, fieldMatcher);
+        private static class AbstractCard_color_Locator extends SpireInsertLocator {
+            public int[] Locate(final CtBehavior ctBehavior) throws CannotCompileException, PatchingException {
+                final Matcher fieldAccessMatcher = new Matcher.FieldAccessMatcher(AbstractCard.class, "color");
+                return LineFinder.findInOrder(ctBehavior, fieldAccessMatcher);
             }
         }
 
-        private static void removeCardFromLibraryCurses(final String cardName) throws NoSuchFieldException, IllegalAccessException {
-            Field field = CardLibrary.class.getDeclaredField("curses");
-            field.setAccessible(true);
-            @SuppressWarnings("unchecked")
-            HashMap<String, AbstractCard> cardLibraryCurses = (HashMap<String, AbstractCard>) field.get(null);
-            cardLibraryCurses.remove(cardName);
+        private static void removeFromCardLibraryCurses(final String cardID) {
+            final HashMap<String, AbstractCard> cardLibraryCurses = ReflectionHacks.getPrivateStatic(CardLibrary.class, "curses");
+            cardLibraryCurses.remove(cardID);
         }
     }
 }
